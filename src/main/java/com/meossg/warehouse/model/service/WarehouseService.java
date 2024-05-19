c7f12fc3ea7c92c3068a2ad0e27a4067e564051b
yoaruku <rhrudgh12@naver.com>
2024-05-19 17:49:56 +0900

    #61 add:Warehouse login

---
+++ src/main/java/com/meossg/warehouse/model/service/WarehouseService.java
@@ -0,0 +1,28 @@
+package com.meossg.warehouse.model.service;
+
+import com.meossg.warehouse.model.dao.WarehouseMapper;
+import com.meossg.warehouse.model.dto.WhAdminDTO;
+import org.apache.ibatis.session.SqlSession;
+
+import static com.meossg.common.MyBatisTemplate.getSqlSession;
+
+public class WarehouseService {
+
+    private WarehouseMapper warehouseMapper;
+    public boolean verifyLogin(String id, String pwd) {
+        SqlSession sqlSession = getSqlSession();
+        warehouseMapper = sqlSession.getMapper(WarehouseMapper.class);
+        WhAdminDTO user = warehouseMapper.getUser(id);
+
+        sqlSession.close();
+
+        if(user == null)
+            return false;
+
+        if (user.getId().equals(id) && user.getPassword().equals(pwd)) {
+            return true;
+        }
+
+        return false;
+    }
+}
