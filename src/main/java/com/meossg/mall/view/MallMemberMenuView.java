fd818d7046c84b9c4a1569c768e7c5a0a71a615f
l2chmnl <l2chmnl@gmail.com>
2024-05-20 11:42:45 +0900

    #19  [2024-05-20] Separate Feat.UpdateMemberInfo

--- src/main/java/com/meossg/mall/view/MallMemberMenuView.java
+++ src/main/java/com/meossg/mall/view/MallMemberMenuView.java
@@ -31,7 +31,7 @@ public class MallMemberMenuView {
                     break;
                 case "2" :
                     // 회원 정보 수정
-                    control.updateMember(inputInfo());
+                    updateMemberMenu();
                     break;
                 case "0" :
                     System.out.println("잘못된 입력입니다.");
@@ -43,6 +43,102 @@ public class MallMemberMenuView {
         }
 
     }
+
+    private void updateMemberMenu() {
+        String memberUpdateMenu = ("""
+                ================
+                회원 정보 수정
+                ----------------
+                1. 비밀번호 변경
+                2. 핸드폰 번호 변경
+                3. 주소 변경
+                4. 전체 변경
+                0. 이전 메뉴로 돌아가기
+                ----------------
+                ================""");
+        while (true) {
+            System.out.println(memberUpdateMenu);
+            System.out.print("변경할 내용을 선택해 주세요 : ");
+            String choice = (new Scanner(System.in).nextLine().charAt(0))+"";
+
+            switch (choice) {
+                case "1" :
+                    control.updatePassword(newPassword());
+                    break;
+                case "2" :
+                    control.updatePhone(newPhone());
+                    break;
+                case "3" :
+                    control.updateAddress(newAddress());
+                    break;
+                case "4" :
+                    control.updateAllInfo(allInfo());
+                    break;
+                case "0" :
+                    return;
+                default:
+                    System.out.println("다시 입력해 주세요.");
+            }
+        }
+    }
+
+    private Map<String, String> allInfo() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("정보를 수정할 회원의 아이디을 입력하세요 : ");
+        String id = sc.nextLine();
+        System.out.print("정보를 수정할 회원의 이름을 입력하세요 : ");
+        String name = sc.nextLine();
+        System.out.print(name + "님의 변경된 비밀번호를 입력하세요 : ");
+        String password = sc.nextLine();
+        System.out.print(name + "님의 변경된 전화번호를 입력하세요 : ");
+        String phone = sc.nextLine();
+        System.out.print(name + "님의 변경된 주소를 입력하세요 : ");
+        String address = sc.nextLine();
+
+        Map<String, String> parameter = new HashMap<>();
+        parameter.put("id", id);
+        parameter.put("name", name);
+        parameter.put("password", password);
+        parameter.put("phone", phone);
+        parameter.put("address", address);
+
+        return parameter;
+    }
+
+    private Map<String, String> newPhone() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("정보를 수정할 회원의 아이디을 입력하세요 : ");
+        String id = sc.nextLine();
+        System.out.print("정보를 수정할 회원의 이름을 입력하세요 : ");
+        String name = sc.nextLine();
+        System.out.print(name + "님의 변경된 전화번호를 입력하세요 : ");
+        String phone = sc.nextLine();
+
+        Map<String, String> parameter = new HashMap<>();
+        parameter.put("id", id);
+        parameter.put("name", name);
+        parameter.put("phone", phone);
+
+        return parameter;
+    }
+
+    private Map<String, String> newAddress() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("정보를 수정할 회원의 아이디을 입력하세요 : ");
+        String id = sc.nextLine();
+        System.out.print("정보를 수정할 회원의 이름을 입력하세요 : ");
+        String name = sc.nextLine();
+        System.out.print(name + "님의 변경된 주소를 입력하세요 : ");
+        String address = sc.nextLine();
+
+        Map<String, String> parameter = new HashMap<>();
+        parameter.put("id", id);
+        parameter.put("name", name);
+        parameter.put("address", address);
+
+        return parameter;
+    }
+
 
     private void showMemberMenu() {
         Scanner sc = new Scanner(System.in);
@@ -86,19 +182,19 @@ public class MallMemberMenuView {
     }
 
 
-    private Map<String, String> inputInfo () {
+    private Map<String, String> newPassword () {
         Scanner sc = new Scanner(System.in);
-        System.out.print("변경할 회원의 이름을 입력하세요 : ");
+        System.out.print("정보를 수정할 회원의 아이디을 입력하세요 : ");
+        String id = sc.nextLine();
+        System.out.print("정보를 수정할 회원의 이름을 입력하세요 : ");
         String name = sc.nextLine();
-        System.out.print(name + "님의 변경된 핸드폰 번호를 입력하세요(- 제외) : ");
-        String phone = sc.nextLine();
-        System.out.print(name + "님의 변경된 주소를 입력해주세요 : ");
-        String address = sc.nextLine();
+        System.out.print(name + "님의 변경된 비밀번호를 입력하세요 : ");
+        String password = sc.nextLine();
 
         Map<String, String> parameter = new HashMap<>();
+        parameter.put("id",id);
         parameter.put("name", name);
-        parameter.put("phone", phone);
-        parameter.put("address", address);
+        parameter.put("password", password);
 
         return parameter;
     }
